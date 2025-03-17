56506138df94ddd89b649a76bfef8cd4b1e3a10f
Md. Akbar Hossain <smakbor33@gmail.com>
2025-03-17 14:27:00 +0600

    Tag

--- BlogWeb/Controllers/TagController.cs
+++ BlogWeb/Controllers/TagController.cs
@@ -36,7 +36,41 @@ namespace BlogWeb.Controllers
             };
             context.Tags.Add(tag1);
             context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult Edit(Guid id)
+        {
+            var tag = context.Tags.FirstOrDefault(x => x.Id == id);
+            if(tag != null)
+            {
+                return View(tag);
+            }
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Edit(Tag tag)
+        {
+            var tag1= context.Tags.FirstOrDefault(x=>x.Id==tag.Id);
+            if (tag1 != null) { 
+                
+                tag1.Name = tag.Name;
+                tag1.DisplayName = tag.DisplayName;
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
             return View(tag);
+        }
+        [HttpPost]
+        public IActionResult Delete(Tag tag)
+        {
+            var tag1= context.Tags.FirstOrDefault(x=>x.Id == tag.Id);
+            if (tag1 != null) {
+             context.Tags.Remove(tag1);
+             context.SaveChanges();
+             return RedirectToAction("Index");
+            }
+            return RedirectToAction("Edit", new {id=tag.Id});     
         }
     }
 }
